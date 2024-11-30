907fad350c9e48577caa6065ad357dacdf47ab3c
Roselin <roselin.t.m@gmail.com>
2024-11-30 14:55:47 -0500

    updated HTML plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
 
 module.exports = {
   entry: {
@@ -33,4 +36,10 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(), // NEW
+  ],
 };
